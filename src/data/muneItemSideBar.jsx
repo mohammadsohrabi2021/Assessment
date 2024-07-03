d261f7262980cb264b0d0c90262bd3fdedbbd9b7
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-07-03 13:23:00 +0330

    create file   muneItemSideBar done

---
+++ src/data/muneItemSideBar.jsx
@@ -0,0 +1,39 @@
+const muneItemSideBar = [
+    {
+      id: 1,
+      title: "لینک های سریع",
+      mune: [
+        { id: 2, name: "داشبورد", href: "/dashboard", icon: "" },
+        { id: 5, name:" پروفایل", href: "/profile", icon: "" },
+        { id: 3, name: "ایجاد کلاس جدید", href: "/createNewClass", icon: "" },
+        { id: 4, name: "کلاس های من", href: "/myClasses", icon: "" },
+        { id: 5, name: "تکالیف من", href: "/myAssignments", icon: "" },
+        
+      ],
+    },
+    {
+      id: 5,
+      title: "برنامه",
+      mune: [
+        { id: 6, name: "برنامه های هفتگی من", href: "/myWeeklyPlans", icon: "" },
+        { id: 7, name: "برنامه آزمون ها", href: "/testSchedule", icon: "" },
+        { id: 8, name: "برنامه امتحانات انلاین", href: "/onlineExamProgram", icon: "" },
+        // { id: 9, name: "ازمون ها", href: "/exams", icon: "" },
+      ],
+    },
+    {
+      id: 10,
+      title: " اشتراک",
+      mune: [{ id: 11, name: "خرید اشتراک", href: "/purchaseSubscription", icon: "" }],
+    },
+    {
+      id: 12,
+      title: "سوالات امتحانی",
+      mune: [
+        { id: 13, name: "سوالات میانترم", href: "/midtermQuestions", icon: "" },
+        { id: 14, name: "سوالات پایان ترم", href: "/end-of-semester-questions", icon: "" },
+      ],
+    },
+   
+  ];
+  export default muneItemSideBar;
