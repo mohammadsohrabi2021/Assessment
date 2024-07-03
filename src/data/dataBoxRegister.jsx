9b20bf2b06fea81bf8674a0a3f3574e883cbaccf
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-07-03 13:17:12 +0330

    create file  dataBoxRegister done

---
+++ src/data/dataBoxRegister.jsx
@@ -0,0 +1,18 @@
+import student from '../assets/images/student.svg'
+import teacher from '../assets/images/teacher.svg'
+const dataBoxRegister = [
+
+    {
+      title: 'استاد',
+      description: 'استاد گرامی، در صورتی که قصد فعالیت بصورت شخصی دارید از این قسمت ثبت نام کنید...',
+      image: teacher ,// جایگزین با آدرس تصویر آیکون واقعی
+      href:'teacher'
+    },
+    {
+      title: 'دانشجو',
+      description: 'دانش آموز گرامی، اگر از طرف مدرسه به سامانه مراجعه کردید...',
+      image: student ,// جایگزین با آدرس تصویر آیکون واقعی
+      href:'student'
+    }
+  ];
+  export default dataBoxRegister
