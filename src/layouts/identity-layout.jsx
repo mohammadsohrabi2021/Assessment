58a8e05063f0040e5188380a912d5ea57a562efc
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-07-03 13:17:54 +0330

    create file  component IdentityLayout done

---
+++ src/layouts/identity-layout.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import HeaderAccount from "../components/HeaderAccount";
+import FooterAccount from "../components/FooterAccount";
+import { Box, Grid } from "@mui/material";
+// import logo from '../assets/images/animated.gif'
+function IdentityLayout() {
+  const myStyle = {
+    backgroundImage:'../assets/images/animated.gif',
+    // height: "100vh",
+    // marginTop: "-70px",
+    fontSize: "50px",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+};
+  return (
+    <Grid container direction="column" style={{ minHeight: '100vh' }} sx={myStyle}  className="IdentityLayout">
+      <HeaderAccount />
+      <Grid item style={{ flexGrow: 1, overflowY: 'auto' }}>
+        <Box  height={'100%'}>
+          <Outlet />
+        </Box>
+      </Grid>
+      <FooterAccount />
+    </Grid>
+  );
+}
+
+export default IdentityLayout;
