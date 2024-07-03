37357ec26cd2383f5a21c678b063880831224654
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2024-07-03 13:10:53 +0330

    create file fieldNames done

---
+++ public/en/FieldNames.js
@@ -0,0 +1,8 @@
+const fieldNames = {
+    name: "نام",
+    lastName: "نام خانوادگی",
+    username: "نام کاربری",
+    password: "رمز عبور",
+    confirmPassword: "تکرار رمز عبور"
+  };
+  export default fieldNames
